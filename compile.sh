4ca5e0ae9b50f9cbdee169afeea5f3370d1ab1a5
Alexander Lizin <sogimu@nxt.ru>
2013-10-06 06:10:32 +0700

    Added Closure compiler as submodule, added test-aplication

--- compile.sh
+++ compile.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-COMPILER_PATH="compiler.jar"
-
 NAME="armengine"
 VERSION="0.1.0"
 RASHIR="js"
 
 FILE_NAME=$NAME-$VERSION.$RASHIR
+FILE_NAME_MIN=$NAME".min"-$VERSION.$RASHIR
 FILE_NAME_WITHOUT_VERSION=$NAME.$RASHIR
 
-#COMPILATION_LEVEL="WHITESPACE_ONLY"
-COMPILATION_LEVEL="SIMPLE_OPTIMIZATIONS"
-#COMPILATION_LEVEL="ADVANCED_OPTIMIZATIONS"
+PATH_TO_COMPILER="./lib/Closure-compiler/"
 
-echo "$FILE_NAME"
-java -jar COMPILER_PATH --js ArmContext/armcontext.js --js ArmGraph/armgraph.js --compilation_level "$COMPILATION_LEVEL" --language_in ECMASCRIPT5 --js_output_file ./$FILE_NAME
+COMPILATION_LEVEL="WHITESPACE_ONLY"
+# COMPILATION_LEVEL="SIMPLE_OPTIMIZATIONS"
+# COMPILATION_LEVEL="ADVANCED_OPTIMIZATIONS"
+
+echo "Building..."
+java -jar "$PATH_TO_COMPILER"compiler.jar --js ArmContext/armcontext.js --js ArmGraph/armgraph.js --compilation_level "$COMPILATION_LEVEL" --language_in ECMASCRIPT5 --js_output_file ./$FILE_NAME_MIN
+
+echo "$FILE_NAME_MIN"
 
 echo "$FILE_NAME_WITHOUT_VERSION"
-cp "$FILE_NAME" "$FILE_NAME_WITHOUT_VERSION"
+cp "$FILE_NAME_MIN" "$FILE_NAME_WITHOUT_VERSION"
+
+echo "$FILE_NAME"
+cp "$FILE_NAME_MIN" "$FILE_NAME"
